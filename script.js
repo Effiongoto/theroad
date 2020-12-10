365de632704d248ce9f2510f9f51ef04857b2299
Effiongoto <effiongoto98@gmail.com>
2020-12-10 13:44:31 +0100

    Created and styled the navigation

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const container = document.querySelector(".container");
+
+
+// click function for the navigation
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+//To apply backgroundcolors to each navigation link
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+//to flip from front to back in the popular tours section
 document.querySelectorAll(".navigation-button").forEach(item => {
   item.onclick = () => {
     item.parentElement.parentElement.classList.toggle("change");
